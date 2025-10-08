c19037b546cb1557a46304ff8bb3f2bc426a7587
Sikolia Malcolm <malcolmsikolia@gmail.com>
2025-10-08 22:44:44 +0300

    updt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
     { name: 'Hotels', href: 'https://hotels.xalleria.com' },
     { name: 'Events', href: 'https://events.xalleria.com' },
     // { name: 'Flight Tickets', href: 'https://air-tickets.xalleria.com' },
-    { name: 'Taxixa', href: 'https://taxi-booking.xalleria.com' },
+   // { name: 'Taxixa', href: 'https://taxi-booking.xalleria.com' },
     { name: 'Safaris', href: 'https://safaris.xalleria.com' },
     // { name: 'Travel Insurance', href: 'https://insurance.xalleria.com' },
     { name: 'Gift Shop', href:'https://shop.xalleria.com' },
